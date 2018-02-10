97ee27c7e1707b2ebd4c3a42f74e052cfaeb1d2f
Mat Pustizzi <mathew.pustizzi@gmail.com>
2018-02-10 18:12:23 -0500

    initial commit

---
+++ gulp/compile.js
@@ -0,0 +1,11 @@
+
+// const nunjucks = require('gulp-nunjucks');
+// const fs = require('fs');
+
+// const pages = JSON.parse( fs.readFileSync('./api/pages.json', { encoding: 'utf8' }));
+
+// const homepage = posts[0]; // for this project we only have one page, index.html
+
+// const res = nunjucks.render('pages/post.html', posts[item]);
+
+// fs.writeFile('dist/' + posts[item].slug + '.html', res);
