bf592c4c9a6581953cef0062ee700f99f3ae24e8
Mat Pustizzi <mathew.pustizzi@gmail.com>
2018-02-12 13:22:11 -0500

    add render task

--- gulp/compile.js
+++ gulp/compile.js
@@ -1,13 +1,23 @@
 
-const nunjucks = require('gulp-nunjucks');
-const fs = require('fs');
+const gulp = require('gulp'),
+	nunjucks = require('nunjucks'),
+	gulpnunjucks = require('gulp-nunjucks'),
+	markdown = require('nunjucks-markdown'),
+    marked = require('marked'),
+	fs = require('fs'),
+	templatesDir = 'src',
+	env = new nunjucks.Environment(new nunjucks.FileSystemLoader(templatesDir));
 
+markdown.register(env, marked);
+	 
 gulp.task('render', () => {
 
-	const pages = JSON.parse( fs.readFileSync('./api/pages.json', { encoding: 'utf8' }));
-		homepage = posts[0]; // for this project we only have one page, index.html
-		res = nunjucks.render('src/index.html', homepage);
+	const pages = JSON.parse( fs.readFileSync('./api/pages.json', { encoding: 'utf8' })),
+		data = pages[0]; // for this project we only have one page, index.html
 
-		fs.writeFile('dist/index.html', res);
-})l
+	gulp.src([templatesDir + '/index.html'])
+        .pipe(gulpnunjucks.compile( data , { env : env }))
+		.pipe(gulp.dest('build'))
+		
+});
 
--- gulp/fetch.js
+++ gulp/fetch.js
@@ -1,7 +1,7 @@
-const contentful = require('contentful'),
+const gulp = require('gulp'),
   env = require('dotenv').config(),
-  gulp = require('gulp'),
-  fs = require('fs'),
+  fs = require('fs')
+  contentful = require('contentful'),
   stringify = require('json-stringify-safe');
   client = contentful.createClient({
     space: process.env.space_id, 
@@ -12,7 +12,7 @@ const contentful = require('contentful'),
 // content type 'page' contains one entry representing th homepage
 // 'page' content type has an id of 'categoryOrder', this needs fixing
 gulp.task('fetch', () =>
-  client.getEntries( { 'content_type': 'categoryOrder' } )
+  client.getEntries( { content_type: 'categoryOrder', include : 10 } )
     .then( (resp) => {
         const data = resp.items.map((item,i) => item.fields);
         fs.writeFileSync('api/pages.json', stringify(data, null, 2)); 
