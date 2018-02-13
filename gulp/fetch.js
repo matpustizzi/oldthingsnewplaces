cd935e0e4285e192005f711e8057adf462afa47f
Mat Pustizzi <mathew.pustizzi@gmail.com>
2018-02-12 20:13:41 -0500

    add media

--- gulp/fetch.js
+++ gulp/fetch.js
@@ -11,11 +11,11 @@ const gulp = require('gulp'),
 // Get the posts data from the cloud CMS and stash it locally
 // content type 'page' contains one entry representing th homepage
 // 'page' content type has an id of 'categoryOrder', this needs fixing
-gulp.task('fetch', () =>
+gulp.task('fetch', () => {
   client.getEntries( { content_type: 'categoryOrder', include : 10 } )
     .then( (resp) => {
         const data = resp.items.map((item,i) => item.fields);
         fs.writeFileSync('api/pages.json', stringify(data, null, 2)); 
       }
-    )
-);
+    );
+});
