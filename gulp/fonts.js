57ac2165c82dfb40cb56540b920fc821c5a77945
Mat Pustizzi <mat.pustizzi@jcrew.com>
2018-02-15 15:27:54 -0500

    add fonts

---
+++ gulp/fonts.js
@@ -0,0 +1,14 @@
+var Fontmin = require('fontmin');
+
+var fontmin = new Fontmin()
+    .src('src/fonts/*.ttf')
+    .dest('build/fonts');
+
+fontmin.run(function (err, files) {
+    if (err) {
+        throw err;
+    }
+
+    console.log(files[0]);
+    // => { contents: <Buffer 00 01 00 ...> }
+});
