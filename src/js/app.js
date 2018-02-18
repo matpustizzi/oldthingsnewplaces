4092f70b814e535edab22643a2cb810290387e34
Mat Pustizzi <mathew.pustizzi@gmail.com>
2018-02-18 10:50:53 -0500

    add working modal slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const domready = require('domready');
 const flickity = require('flickity');
+const domready = require('domready');
 
 domready(function(){
     const slider = new flickity( '.slider', {
@@ -10,38 +10,6 @@ domready(function(){
         prevNextButtons: false
     });
 
-    const modalSlider = new flickity( '.js-modal-slider', {
-        lazyLoad : 2,
-        pageDots: false,
-        prevNextButtons: true
-    });
-
-    const projects = document.getElementsByClassName('js-project');
-    const modal = document.querySelector('.js-modal');
-
-    Array.from(projects).forEach((element) => {
-        element.addEventListener('click', function(e) {
-          openModal(this.dataset.slides);
-        });
-    });
-
-    const openModal = (slides) => {
-        if(slides){
-            modal.classList.add('modal--visible');
-        }
-    }
-
-    const closeModal = () => {
-        modal.classList.remove('modal--visible');
-        var cells = modalSlider.getCellElements();
-        modalSlider.remove(cells);
-    }
-
-    modal.addEventListener('click',(e) => {
-        if(e.target.classList.contains('js-modal')) {
-            closeModal();
-        }
-        
-    })
+    require('./modal');
     
 });
---
+++ src/js/modal.js
@@ -0,0 +1,62 @@
+const flickity = require('flickity');
+require('flickity-imagesloaded');
+
+const modalSlider = new flickity( '.js-modal-slider', {
+    lazyLoad: 1,
+    pageDots: false,
+    prevNextButtons: true,
+    percentPosition: true
+});
+
+const projects = document.getElementsByClassName('js-project');
+const modal = document.querySelector('.js-modal');
+const captionTitle = document.querySelector('.modal-caption__title');
+const captionText = document.querySelector('.modal-caption__text');
+
+
+Array.from(projects).forEach((element) => {
+    element.addEventListener('click', function(e) {
+      openModal(this.dataset.slides);
+      setModalTitle(this.dataset.project);
+    });
+});
+
+const renderSlides = (slides) => {
+    return slides.map( (slide,i) => {
+        var el = document.createElement('div');
+        el.className = 'modal-slider__slide'
+        el.innerHTML = `<img class="js-slide-image modal-slider__image" data-flickity-lazyload="${ slide.file.url }" ${ slide.description ? `alt="${ slide.description }"` : '' }>`
+        return el;
+    } )
+}
+
+const openModal = (slides) => {
+    var data = JSON.parse(slides)
+    if(data){
+        modal.classList.add('modal--visible');
+        modalSlider.prepend(renderSlides(data));
+        modalSlider.resize();
+        //modalSlider.reposition();
+    }
+}
+
+const setModalTitle = (title) => {
+    captionTitle.textContent = title;
+}
+
+const caption = document.querySelector('.js-modal-caption')
+const closeModal = () => {
+    modal.classList.remove('modal--visible');
+    var cells = modalSlider.getCellElements();
+    modalSlider.remove(cells);
+}
+
+modal.addEventListener('click',(e) => {
+    if(e.target.classList.contains('js-modal')) {
+        closeModal();
+    }
+});
+
+modalSlider.on( 'select', function() {
+    captionText.textContent = modalSlider.selectedElement.querySelector('.js-slide-image').alt;
+});
