2ab727d8acc9f766ea3670e33505fde1f8652dc9
Mat Pustizzi <mat.pustizzi@jcrew.com>
2018-02-22 12:33:35 -0500

    add preview option to fetch

--- gulp/fetch.js
+++ gulp/fetch.js
@@ -4,21 +4,25 @@ const gulp = require('gulp'),
   fs = require('fs'),
   contentful = require('contentful'),
   stringify = require('json-stringify-safe'),
-  client = contentful.createClient({
+  opts = {
     space: process.env.space_id, 
-    accessToken: process.env.api_key
-  });
-
+    accessToken: process.env.api_key,
+    host: `${ process.env.preview ? 'preview' : 'cdn' }.contentful.com`
+  },
+  client = contentful.createClient(opts);
+  
 // content type 'page' contains one entry representing th homepage
 // page content type has slug 'categoryOrder', this needs fixing
 gulp.task('fetch', () => {
-  var req = client.getEntries( { content_type: 'categoryOrder', include : 10 } )
+  var req = client.getEntries( { 
+    content_type: 'categoryOrder',
+    include : 10 } )
     .then((resp) => {
         const data = resp.items.map((item,i) => item.fields);
         fs.writeFileSync('api/pages.json', stringify(data, null, 2)); 
       }
     ).catch((err) => {
-      gutil.log("Error: " + err.code)
+      gutil.log("Error: " + err.message)
       process.exit(1)
     });
     
